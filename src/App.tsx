9196e2e2367f9b4f59b99cd5a0971aa88514af78
Roman <roma.romanvov.99@mail.ru>
2023-08-11 17:41:55 +0300

    Фикс для отображения нескольких GraphView

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,48 @@ import { GraphModel } from './graph-model';
 import './App.css';
 
 function App() {
+  let sampleGraphModel: GraphModel = new GraphModel(
+    [
+      {label:"Узел 1.1", pos:[70, 70], color:"#AA1122"}, 
+      {label:"Узел 2.1", pos:[130, 190], color:"#11DD33"},  
+      {label:"Узел 2.2", pos:[180, 290], color:"#11DD33"}, 
+      {label:"Узел 3.1", pos:[220, 120], color:"#0022FF"}, 
+      {label:"Узел 3.2", pos:[300, 30], color:"#AAAA00"}, 
+      {label:"Узел 3.3", pos:[350, 100], color:"#AAAA00"}, 
+      {label:"Узел 4.1", pos:[250, 180], color:"#0022FF"}, 
+      {label:"Узел 4.2", pos:[290, 260], color:"#0022FF"}, 
+      {label:"Узел 5.1", pos:[450, 70], color:"#CC7700"}, 
+      {label:"Узел 5.2", pos:[420, 180], color:"#CC7700"}
+    ],
+    [
+      {from:0, to:1}, 
+      {from:1, to:2},
+      {from:1, to:3},
+      {from:2, to:6},
+      {from:2, to:7},
+      {from:3, to:4},
+      {from:3, to:5},
+      {from:5, to:8},
+      {from:5, to:9}
+    ]);
+
+    let sampleGraphModel1: GraphModel = new GraphModel(
+      [
+        {label:"Узел 1.1", pos:[70, 70], color:"#AA1122"}, 
+        {label:"Узел 2.1", pos:[130, 190], color:"#11DD33"},  
+        {label:"Узел 2.2", pos:[180, 290], color:"#11DD33"}, 
+        {label:"Узел 3.1", pos:[220, 120], color:"#0022FF"}, 
+        {label:"Узел 3.2", pos:[300, 30], color:"#AAAA00"}, 
+        {label:"Узел 3.3", pos:[350, 100], color:"#AAAA00"}
+      ],
+      [
+        {from:0, to:1}, 
+        {from:1, to:2},
+        {from:1, to:3},
+        {from:3, to:4},
+        {from:3, to:5},
+      ]);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -11,33 +53,25 @@ function App() {
       </header>
       <main>
         <GraphView 
-          model={new GraphModel(
-          [
-            {label:"Узел 1.1", pos:[70, 70], color:"#AA1122"}, 
-            {label:"Узел 2.1", pos:[130, 190], color:"#11DD33"},  
-            {label:"Узел 2.2", pos:[180, 290], color:"#11DD33"}, 
-            {label:"Узел 3.1", pos:[220, 120], color:"#0022FF"}, 
-            {label:"Узел 3.2", pos:[300, 30], color:"#AAAA00"}, 
-            {label:"Узел 3.3", pos:[350, 100], color:"#AAAA00"}, 
-            {label:"Узел 4.1", pos:[250, 180], color:"#0022FF"}, 
-            {label:"Узел 4.2", pos:[290, 260], color:"#0022FF"}, 
-            {label:"Узел 5.1", pos:[450, 70], color:"#CC7700"}, 
-            {label:"Узел 5.2", pos:[420, 180], color:"#CC7700"}
-          ],
-          [
-            {from:0, to:1}, 
-            {from:1, to:2},
-            {from:1, to:3},
-            {from:2, to:6},
-            {from:2, to:7},
-            {from:3, to:4},
-            {from:3, to:5},
-            {from:5, to:8},
-            {from:5, to:9}
-          ])}
-          captionHeader={"Тестовый граф"}
-          viewWidthPx={800}
-          viewHeightPx={648}
+          model={sampleGraphModel}
+          viewWidthPx={640}
+          viewHeightPx={480}
+        />
+        <GraphView 
+          model={sampleGraphModel}
+          viewWidthPx={640}
+          viewHeightPx={480}
+          captionHeader="Sample view"
+        />
+        <GraphView 
+          model={sampleGraphModel1}
+          viewWidthPx={640}
+          viewHeightPx={480}
+        />
+        <GraphView 
+          model={sampleGraphModel}
+          viewWidthPx={640}
+          viewHeightPx={480}
         />
       </main>
       <footer>
